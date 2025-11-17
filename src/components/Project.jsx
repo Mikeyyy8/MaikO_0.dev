33acbb0314e95cf53ed22726fc43ca178cbdd0a4
Glory Michael <mikeyyyglo12@gmail.com>
2025-11-17 17:55:58 +0100

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,7 @@
 const Project = ({ name, url, dscr, src }) => {
   return (
     <div className="border border-neutral-800 rounded-lg p-2 hover:bg-neutral-900 duration-200">
-      <a href={url} className="flex items-center gap-4">
+      <a href={url} target="_blank" rel="noopener noreferrer" className="flex items-center gap-4">
         <div className="border border-neutral-800 h-10 w-10 rounded">
           <img
             src={src ? src : `https://placehold.co/50x50?text=${name.slice(0,2)}`}
